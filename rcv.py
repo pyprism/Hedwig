7720ad2ff9fc11f3a7e8df7017ec327f1eada598
Ashutosh Das <git.pyprism@gmail.com>
2025-06-11 22:00:46 +0600

    added bucket and sqs cleanup

--- rcv.py
+++ rcv.py
@@ -9,7 +9,7 @@ SQS_QUEUE_URL = ""
 S3_BUCKET = ''
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-AWS_DEFAULT_REGION = ''
+AWS_DEFAULT_REGION = 'us-east-1'
 
 sqs = boto3.client('sqs', region_name=AWS_DEFAULT_REGION,
                        aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -24,18 +24,34 @@ def process_email_from_s3(s3_bucket, s3_key):
     obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)
     raw_email = obj["Body"].read()
     msg = email.message_from_bytes(raw_email, policy=policy.default)
-
+    print("raw body", msg)
     subject = msg["subject"]
     sender = msg["from"]
     to = msg["to"]
     body = msg.get_body(preferencelist=('plain', 'html')).get_content()
-    print("$$$$$$$$\n")
     print("====== Email Received ======")
     print("From:", sender)
     print("To:", to)
     print("Subject:", subject)
     print("Body:\n", body[:500])
     print("============================")
+
+def delete_email_from_s3(bucket, key):
+    try:
+        s3.delete_object(Bucket=bucket, Key=key)
+        print(f"Deleted: s3://{bucket}/{key}")
+    except Exception as e:
+        print(f"Failed to delete {key}: {e}")
+
+def delete_message_from_sqs(receipt_handle):
+    try:
+        sqs.delete_message(
+            QueueUrl=SQS_QUEUE_URL,
+            ReceiptHandle=receipt_handle
+        )
+        print("Message deleted from SQS")
+    except Exception as e:
+        print("Failed to delete message from SQS:", e)
 
 def poll():
     print("Starting to poll SQS for messages...")
@@ -58,14 +74,13 @@ def poll():
                 print("Processing S3 key:", s3_key)
                 process_email_from_s3(S3_BUCKET, s3_key)
                 print("Processed message:", message["MessageId"])
+                delete_email_from_s3(S3_BUCKET, s3_key)
         except Exception as e:
             print("Error:", e)
 
         # delete message after processing
-        # sqs.delete_message(
-        #         QueueUrl=SQS_QUEUE_URL,
-        #         ReceiptHandle=message["ReceiptHandle"]
-        # )
+        delete_message_from_sqs(message["ReceiptHandle"])
+
 
         # Short sleep before next poll
         #time.sleep(2)
