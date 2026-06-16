f6bed905149458d6b841e60b583713e3b1580930
Ashutosh Das <git.pyprism@gmail.com>
2026-06-16 21:28:53 +0600

    changed exception handling in IP resolution to return empty list on OSError

--- utils/ip.py
+++ utils/ip.py
@@ -8,6 +8,6 @@ def get_local_ip():
         resolved_ips = socket.gethostbyname_ex(hostname)[2]
         _internal_ips += resolved_ips
         _internal_ips += [ip.rsplit(".", 1)[0] + ".1" for ip in resolved_ips]
-    except Exception as e:
-        raise RuntimeError(f"Failed to resolve local IP addresses: {e}")
+    except OSError:
+        return _internal_ips
     return _internal_ips
