9032764028b5a5fb06375047c5da99b754b38943
Ashutosh Das <git.pyprism@gmail.com>
2026-06-27 20:40:52 +0600

    added post-delete signal for EmailAttachment to enqueue S3 cleanup; improved attachment file management after deletion

--- hedwig/signals.py
+++ hedwig/signals.py
@@ -1,9 +1,10 @@
+from django.db import transaction
 from django.db.models import F, Value
 from django.db.models.functions import Greatest
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from hedwig.models import EmailMessage, Mailbox
+from hedwig.models import EmailAttachment, EmailMessage, Mailbox
 
 
 @receiver(post_delete, sender=EmailMessage)
@@ -14,3 +15,20 @@ def decrement_mailbox_used_bytes(sender, instance, **kwargs):
     Mailbox.objects.filter(pk=instance.mailbox_id).update(
         used_bytes=Greatest(F("used_bytes") - instance.size_bytes, Value(0))
     )
+
+
+@receiver(post_delete, sender=EmailAttachment)
+def delete_attachment_file(sender, instance, **kwargs):
+    """Enqueue S3 cleanup for an attachment after the DB delete commits."""
+    if not instance.file:
+        return
+
+    storage_key = instance.storage_key
+    file_url = instance.file
+
+    def enqueue_cleanup():
+        from hedwig.tasks import delete_unreferenced_attachment_file_task
+
+        delete_unreferenced_attachment_file_task.delay(file_url, storage_key)
+
+    transaction.on_commit(enqueue_cleanup)
